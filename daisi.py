654dea8171b93199a1f76320773bca3c2b48ef64
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-05 12:32:27 +0530

    Delete daisi.py

--- daisi.py
+++
@@ -1,8 +0,0 @@
-import pydaisi as pyd
-info_crypto = pyd.Daisi("farhun/INFOCRYPTO-CRYPTOAPP")
-info_crypto.build_dce_net(image_size=None).value
-info_crypto.get_model().value
-info_crypto.(i).value
-info_crypto.st_ui().value
-import webbrowser
-webbrowser.open('https://infocryptos.netlify.app/')
